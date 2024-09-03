ffcafbe7cce82ee0d250a9378c2424009427bb8d
Dominick Smith <dominick.smith938@gmail.com>
2024-09-03 13:41:25 -0500

    added download queue

--- respotdl.py
+++ respotdl.py
@@ -1,6 +1,17 @@
 import os
 
-link = str(input("Enter the URL you wish to download:\n"))
+links = []
+url_prompt = True
 
-os.system(f"spotdl download {
-          link} --output ./{{album-artist}}/{{album}}/{{title}}")
+while url_prompt:
+    link = str(input("Enter the URL of your spotify song/album/playlist: "))
+    print("Type 'done' to start downloading")
+    if link.lower() == "done":
+        url_prompt = False
+    else:
+        links.append(link)
+
+
+for url in links:
+    os.system(f"spotdl/bin/spotdl download {url} --output ./out/{{album-artist}}/{{album}}/{{title}}")
+print("----The download process has completed----")
